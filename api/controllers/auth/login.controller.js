e4b595742463850100f92dff547763730e87fdf2
candidcodes <candidpradhan@gmail.com>
2024-07-05 17:15:07 +0545

    some changes

--- api/controllers/auth/login.controller.js
+++ api/controllers/auth/login.controller.js
@@ -1,5 +1,21 @@
+const { validationError, errorMsg } = require("@/lib")
+const { User } = require("@/models")
+
 class LoginCtrl{
     login = async (req, res, next) => {
+        try{
+            const {email, password} = req.body
+
+            const user = await User.findOne({email})
+
+            if(user){
+
+            }else{
+                validationError (next, {email: "Given Email is not registered"})
+            }
+        }catch(error){
+            errorMsg(next, error)
+        }
 
     }
 }
