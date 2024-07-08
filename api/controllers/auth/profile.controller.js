b2638c99276026045b434d58a9069055b5b39a23
candidcodes <candidpradhan@gmail.com>
2024-07-08 16:47:57 +0545

    update

--- api/controllers/auth/profile.controller.js
+++ api/controllers/auth/profile.controller.js
@@ -1,10 +1,22 @@
+const { errorMsg } = require("@/lib")
+const { User } = require("@/models")
+
 class profileCtrl {
     detail = async(req, res, next) => {
         res.send(req.user)
     }
     
     update = async(req, res, next) => {
-        res.send('')
+        try{
+            const { name, phone, address } = req.body
+
+            await User.findByIdAndUpdate(req.user._id, {name, phone, address})
+            res.send({
+                message: 'profile updated'
+            })
+        }catch(error){
+            errorMsg(next, error)
+        }
     }
 
     password = async(req, res, next) => {}
